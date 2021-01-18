544f9aa725fe2834c60a72d6cca95c7a8beed163
zzuob <af19846@bristol.ac.uk>
2021-01-18 00:26:57 +0000

    add main.py, move main functions from logsimp.py to main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+from logsimp import yn_input, min_in, bool_in, gic, tt_out
+
+if __name__ == "__main__":
+    input_type=yn_input('Define expression with minterms? [y/n]\n')
+    if input_type == 'y':
+        SOP, POS = min_in()
+    else:
+        SOP, POS = bool_in()
+
+    print('SOP is {0} with cost = {1} gi'.format(SOP, gic(SOP)))
+    print('POS is {0} with cost = {1} gi'.format(POS, gic(POS)))
+
+    input_type=yn_input('Show truth table? [y/n]\n')
+    if input_type == 'y':
+        tt_out(str(SOP))
